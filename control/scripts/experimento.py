1aea56d36646c78ea6f528e7460a56da56b5be0e
Gabriel Maciá <gmacia@ugr.es>
2015-12-24 16:46:38 +0100

    Modify experiment control script and rename it to VeritasExperiment (class) and experimento.py

--- control/scripts/experimento.py
+++ control/scripts/experimento.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python
 # -*- coding: cp1252 -*-
 
-# Autor: Gabriel Maciá
-# Fecha: 14/12/2015
+# Autor: Gabriel MaciÃ¡
+# Fecha: 23/12/2015
 
-import sys
-import logging
-import argparse
-import time
-import datetime
-import subprocess
-from threading import Thread
+
+from lib.VeritasExperiment import VeritasExperiment
+
 
 ##########################################
-# DEFINICION DE VARIABLES Y CONSTANTES
-##########################################
-
-LOGFILENAME = 'DoS_attack.log'
-LOGLEVEL = logging.DEBUG
-LOGDIR = '../log/'
-
 # El formato de la lista de comandos es: [tInicio, comando]
+
+TRAFFIC_GENERATOR = 'cd git/VERITASExperimentalScripts/machines/clients/scripts/; ./trafficGenerator.py '
+
 lista_comandos = [
-                  [1, 'ssh gmacia@192.168.56.104 echo prueba1'], #Comentario para la ejecución
-                  [1, 'ssh gmacia@192.168.56.104 echo prueba2'],
+                  [4, 'ssh nesg@routerR1 "echo prueba1"'], #Comentario para la ejecuciÃ³n
+                  [1, 'ssh nesg@routerR2 "hostname; sleep 2; echo prueba2"'],
                   [2, 'hostname; date']              
                   
                   
@@ -34,65 +26,21 @@ comandos_procesado = [
                       ]
 
 
+httpTraffic = [ [0, 'echo "Inicio de comandos de preprocesado (limpieza y demas)"'],
+				[1, 'ssh nesg@m1.2 "' + TRAFFIC_GENERATOR + ' -n 10 -t 5 > tmp.txt"'],
+				[3, 'ssh nesg@m2.2 "' + TRAFFIC_GENERATOR + ' -n 10 -t 5"']
+				
+				]	
+
+
 ##########################################
 
 
-
-
-
-
-
-def execute_command (instanteEjecucion, command):
-    
-    time.sleep(instanteEjecucion)
-    tInicial = time.strftime('%X')
-    beginInterval= datetime.datetime.now()
-    output = subprocess.check_output(command, shell=True).decode('utf-8')
-    tFinal = time.strftime('%X')
-    endInterval = datetime.datetime.now()
-    interval = endInterval - beginInterval
-     
-    cabecera = ('   -->(result)$ ' + 
-              command +
-              ' (' +
-              str(interval.seconds/3600)+'d'+str(interval.seconds%3600/60)+'m'+str(interval.seconds%60)+'s' + 
-              '---' + 
-              tInicial +
-              '-' + 
-              tFinal + 
-              ')'
-              )
-
-    log.info (cabecera + '\n\t\t\t\t\t\t\t|' + output.strip().replace('\n','\n\t\t\t\t\t\t\t|'))
-    
- 
-
 if __name__ == '__main__':
+	
+	e = VeritasExperiment ("experimentoPrueba")
 
-    #global lista_comandos
-    logging.basicConfig(
-                        #filename=LOGDIR + time.strftime('%y%m%d_%H.%M.%S_')+ LOGFILENAME,
-                        level=logging.DEBUG,
-                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
-                        )
-    log = logging.getLogger("MSNM_main")
-
-    # Ejecución de los comandos cada uno en un thread
-    
-    log.info ('[+] Inicio de ejecución de comandos')
-    threads = []
-    for twait, command in lista_comandos: 
-        t = Thread(target=execute_command, args=(twait, command,))
-        threads.append(t)
-        t.start()
-
-    for t in threads:
-        t.join()
-
-
-    log.info ('[+] Inicio de procesado: ' + time.strftime('%X %d/%m/%y'))
-
-    for command in comandos_procesado:
-        execute_command(0, command)
-
-    log.info ('[+] Final de procesado: ' + time.strftime('%X %d/%m/%y'))    
+	e.insertaComandoEjecucion([5, "ls"])
+	e.insertaListaComandosEjecucion(httpTraffic)
+	e.imprimeComandos()
+	e.execute()
---
+++ control/scripts/lib/VeritasExperiment.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python
+# -*- coding: cp1252 -*-
+
+# Autor: Gabriel Maciá
+# Fecha: 14/12/2015
+
+import sys
+import logging
+import argparse
+import time
+import datetime
+import subprocess
+from threading import Thread
+
+##########################################
+# DEFINICION DE VARIABLES Y CONSTANTES
+##########################################
+
+#LOGFILENAME = 'DoS_attack.log'
+#LOGLEVEL = logging.DEBUG
+#LOGDIR = '../log/'
+
+
+
+
+
+class VeritasExperiment:
+
+	def __init__(self, experimentName="veritasExperiment"):
+		self.comandosEjecucion = []
+		self.comandosProcesado = []
+		self.experimentName = experimentName
+		logging.basicConfig(
+                        #filename=LOGDIR + time.strftime('%y%m%d_%H.%M.%S_')+ LOGFILENAME,
+                        level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+                        )
+		self.log = logging.getLogger(experimentName)
+
+
+	def esComando (self, comando):
+		# Comprueba si el comando tiene el formato correcto
+		if isinstance (comando, list) and isinstance(comando[0],int) and isinstance(comando[1],str):
+			return True
+		else:
+			return False
+
+	def insertaListaComandosEjecucion (self, listaComandos):
+		if isinstance(listaComandos, list):
+			for comando in listaComandos:
+				self.insertaComandoEjecucion(comando)
+
+	def insertaListaComandosProcesado (self, listaComandos):
+		if isinstance(listaComandos, list):
+			for comando in listaComandos:
+				self.insertaComandoProcesado(comando)
+
+	def insertaComandoEjecucion (self, comando):
+		if self.esComando(comando):
+			if comando[0]<0:
+				comando[0] = 0
+			self.comandosEjecucion.append(comando)
+			self.ordenaListaComandos (self.comandosEjecucion)
+		
+		else:
+			print 'Comando no insertado por error: ' + str(comando)
+	
+	def insertaComandoProcesado (self, comando):
+		# Comprueba si el comando tiene el formato correcto
+		if self.esComando(comando):
+			if comando[0]<0:
+				comando[0] = 0
+			self.comandosProcesado.append(comando)
+			self.ordenaListaComandos (self.comandosProcesado)
+
+		else:
+			print 'Comando no insertado por error: ' + str(comando)
+
+		
+	def ordenaListaComandos(self, lista): 
+		lista.sort(key=lambda x: x[0])
+		
+	def imprimeComandos (self):
+		print '------------------------------------'
+		print 'Lista de comandos de ejecucion:'
+		for comando in self.comandosEjecucion:
+			print "t=" + str(comando[0]) + "s\t" + str(comando[1])
+		print '\nLista de comandos de procesado:'
+		for comando in self.comandosProcesado:
+			print "t=" + str(comando[0]) + "s\t" + str(comando[1])
+		print '------------------------------------\n'
+
+
+	def executeCommand (self, instanteEjecucion, command):
+		
+		log = self.log
+		
+		time.sleep(instanteEjecucion)
+		tInicial = time.strftime('%X')
+		beginInterval= datetime.datetime.now()
+		output = subprocess.check_output(command, shell=True).decode('utf-8')
+		tFinal = time.strftime('%X')
+		endInterval = datetime.datetime.now()
+		interval = endInterval - beginInterval
+		 
+		cabecera = ('   -->(result)$ ' + 
+				  command +
+				  ' (' +
+				  str(interval.seconds/3600)+'d'+str(interval.seconds%3600/60)+'m'+str(interval.seconds%60)+'s' + 
+				  '---' + 
+				  tInicial +
+				  '-' + 
+				  tFinal + 
+				  ')'
+				  )
+
+		log.info (cabecera + '\n\t\t\t\t\t\t\t|' + output.strip().replace('\n','\n\t\t\t\t\t\t\t|'))
+
+		
+	 
+
+	def execute (self):
+
+
+
+		# Ejecución de los comandos cada uno en un thread
+		log = self.log
+		
+		log.info ('[+] Inicio de ejecucion de comandos')
+		threads = []
+		for twait, command in self.comandosEjecucion: 
+			t = Thread(target=self.executeCommand, args=(twait, command,))
+			threads.append(t)
+			t.start()
+
+		for t in threads:
+			t.join()
+
+
+		log.info ('[+] Inicio de procesado: ' + time.strftime('%X %d/%m/%y'))
+
+		for comando in self.comandosProcesado:
+			executeCommand()
+
+		log.info ('[+] Final de procesado: ' + time.strftime('%X %d/%m/%y'))    
+
+
+if __name__ == '__main__':
+	
+	exp = VeritasExperiment() 
