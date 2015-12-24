b5e200f77406651a565b2230c27f1a73405ce1f5
Gabriel Macia <gmacia@ugr.es>
2015-12-24 11:55:38 +0100

    Corrige error en path de fichero install_wwwserver.sh

--- machines/wwwserver/install_wwwserver.sh
+++ machines/wwwserver/install_wwwserver.sh
@@ -3,4 +3,4 @@
 # Este script copia las paginas web que los clientes visitaran en el directorio
 # que corresponde al servidor
 
-sudo cp /html_files/* /var/www/html
+sudo cp ./html_files/* /var/www/html
