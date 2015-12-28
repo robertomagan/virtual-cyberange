58f7599305c6d2f2b4877c615abb7c85150a3bee
Gabriel Maciá <gmacia@ugr.es>
2015-12-28 08:25:51 +0100

    Modify plot to include dates in the x-axis

--- control/scripts/plot.py
+++ control/scripts/plot.py
@@ -24,24 +24,32 @@ for file in args.fileName:
     if l == 'No matched flows\n':
         print "[-] Fichero vacio: " + file
     else:
+        print '[+] Procesando fichero: ' + file
+        # Leemos el fichero .csv, poniendo como indice la columna con la fecha para poder después hacer el 
+        # resampling con el metodo resample. 
         df = pd.read_csv (file, parse_dates={'date': [0]}, header=None, usecols=[0], index_col=[0])
         df[file[:-4]] = 1
-        df = df.resample('2Min', how='sum').reset_index()
+        # Para poder hacer resample, df debe tener como indice las fechas. Después de hacerlo reseteamos el indice
+        # para poder llamar posteriormente a merge con la columna date. 
+        df = df.resample('1Min', how='sum').reset_index()
  #       print df.head()
         dflist.append(df)
-        print '[+] Procesado fichero: ' + file
+        print 'Ok'
 
-#print "Longitud dflist: " + str(len(dflist))
+# Dependiendo del numero de ficheros procesados, hacemos un merge de todos o no. Al final es importante
+# volver a colocar el indice con la columna date, para que la gráfica muestre las fechas. 
 
 if len(dflist)>1:
-    df_final = reduce(lambda left,right: pd.merge(left,right,on='date',how='outer'), dflist).fillna(0)
+    # Agrupamos los dataframe de todos los ficheros, rellenando con 0s los NaN y poniendo al final
+    # el indice a la columna date para que aparezca en la grafica en el eje x. 
+    df_final = reduce(lambda left,right: pd.merge(left,right,on='date',how='outer'), dflist).fillna(0).set_index('date')
      
 elif len(dflist) == 1:
-    df_final = dflist[0]
+    df_final = dflist[0].set_index('date')
 else:
     print "No hay datos validos en los ficheros indicados"
     exit()
     
-#print df_final.head()
+print df_final
 df_final.plot(marker='o')
 plt.show()
