ce627552b906e44c3b841e9247e6366df83104c2
Gabriel Maciá <gmacia@ugr.es>
2015-12-29 18:41:10 +0100

    Correct bug in nmapGenerator.py related to args.destination

--- attacks/nmapGenerator.py
+++ attacks/nmapGenerator.py
@@ -8,15 +8,15 @@ import argparse
 
 parser = argparse.ArgumentParser (description='Execute a nmap scan against a destination (network or host')
 parser.add_argument(dest='destination', metavar='destination', action='store', help='IP address/segment of the victims', nargs=1)
-parser.add_argument('-i', dest='interval', metavar='interval', action='store', default=300, help='interval(seconds) between consecutive scans. Default 5 min')
-parser.add_argument('-d', dest='duration', metavar='duration', action='store', default=3600, help='total duration (seconds) of the scan process. Default 1h')
+parser.add_argument('-i', dest='interval', metavar='interval', action='store', type=int, default=300, help='interval(seconds) between consecutive scans. Default 5 min')
+parser.add_argument('-d', dest='duration', metavar='duration', action='store', type=int, default=3600, help='total duration (seconds) of the scan process. Default 1h')
 
 args = parser.parse_args()
 
 
 duration = args.duration # Seconds (default 1 hour)
 interval = args.interval #Seconds (default 5 minutes)
-nmapCommand = 'nmap -A ' + args.destination 
+nmapCommand = 'nmap -A ' + args.destination[0]
 
 iteraciones = 0
 
