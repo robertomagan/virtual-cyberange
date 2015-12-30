46adc400f2172b227004739693e9529e1952f83c
Gabriel Maciá <gmacia@ugr.es>
2015-12-30 10:35:15 +0100

    modify duration calculation

--- attacks/nmapGenerator.py
+++ attacks/nmapGenerator.py
@@ -5,23 +5,33 @@
 import time
 import os
 import argparse
+import datetime
 
 parser = argparse.ArgumentParser (description='Execute a nmap scan against a destination (network or host')
 parser.add_argument(dest='destination', metavar='destination', action='store', help='IP address/segment of the victims', nargs=1)
-parser.add_argument('-i', dest='interval', metavar='interval', action='store', type=int, default=300, help='interval(seconds) between consecutive scans. Default 5 min')
+parser.add_argument('-i', dest='interval', metavar='interval', action='store', type=int, default=300, help='interval(seconds) between the end of a scan and the start of the next scans. Default 5 min')
 parser.add_argument('-d', dest='duration', metavar='duration', action='store', type=int, default=3600, help='total duration (seconds) of the scan process. Default 1h')
 
 args = parser.parse_args()
 
 
-duration = args.duration # Seconds (default 1 hour)
+duration = datetime.timedelta(seconds=args.duration) # Seconds (default 1 hour)
 interval = args.interval #Seconds (default 5 minutes)
 nmapCommand = 'sudo nmap -n -sS ' + args.destination[0]  # -n: no DNS resolution, -sS SYN Stealth scan 
 
-iteraciones = 0
 
-while iteraciones*interval < duration:
+inicio = datetime.datetime.now()
+
+iteraciones = 0
+while datetime.datetime.now()-inicio < duration:
+	if (iteraciones != 0):
+		print 'Transcurridos ' + str((datetime.datetime.now()-inicio).seconds)+ ' segundos.'
+		print 'En espera durante: ' + str(interval)
+		time.sleep(interval)
+	
+	iteraciones = iteraciones+1
+	print 'Ejecutando: ' + nmapCommand
 	os.system(nmapCommand)
-	time.sleep (interval)
+	print '--> Ejecutado. '
 	
 
