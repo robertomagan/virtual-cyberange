c8ffcafc15fb8863418542f65461363f26622668
robertomagan <rmagan@ugr.es>
2017-08-10 10:53:32 +0200

    Adding remote command to remove the current project dir ...

--- control/scripts/gitclone.sh
+++ control/scripts/gitclone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Este script realiza un git clone recursivo en el directorio ~/git/ para una rama en concreto
+# Este script realiza un borrado del proyecto y luego un git clone recursivo en el directorio ~/git/ para una rama en concreto
 # de todas las maquinas en la lista de maquinas de VERITAS
 
 if [ "$#" -ne 1 ]; then
@@ -17,14 +17,11 @@ fi
 #maquinas=(atacante borderRouter routerR1 routerR2 routerR3 m1.2 m2.2 m3.2 dmz wwwserver)
 maquinas=(wwwserver)
 DIR='git'
-project_DIR='git/VERITASExperimentalScripts'
+project_DIR='VERITASExperimentalScripts'
 branch=$1
-
-# removing the current project
-rm -rf $project_DIR
 
 for m in ${maquinas[*]}
 do
 	printf "[+] Connecting to: %s\n" $m
-	eval $(printf "ssh nesg@%s \"cd %s; git clone --recursive git@metis.ugr.es:gmacia/VERITASExperimentalScripts -b %s\"\n" $m $DIR $branch)
+	eval $(printf "ssh nesg@%s \"cd %s; rm -rf %s; git clone --recursive git@metis.ugr.es:gmacia/VERITASExperimentalScripts -b %s\"\n" $m $DIR $project_DIR $branch)
 done
